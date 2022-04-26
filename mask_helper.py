ec62e0b16f67e09b9e2c11934360e86eb2c48324
Cường Nguyễn <cuong.nguyen4@be.com.vn>
2022-04-26 11:06:03 +0700

    add beAI mask

--- mask_helper.py
+++ mask_helper.py
@@ -4,10 +4,12 @@ import cv2
 import numpy as np
 from PIL import Image
 
-logo_s_list=[]
+logo_s_list = []
 logo_s_list.append(cv2.imread("asset/mask1.png", cv2.IMREAD_UNCHANGED))
 logo_s_list.append(cv2.imread("asset/mask2.png", cv2.IMREAD_UNCHANGED))
 logo_s_list.append(cv2.imread("asset/mask3.png", cv2.IMREAD_UNCHANGED))
+logo_s_list.append(cv2.imread("asset/mask4.png", cv2.IMREAD_UNCHANGED))
+
 
 def smooth_edge(img, pixel_smooth):
     # pixel_smooth=300
@@ -23,16 +25,18 @@ def zoom_scale(img, pixel_crop):
     im_pil = im_pil.resize((512, 512), Image.ANTIALIAS)
     return np.asarray(im_pil)
 
+
 def rotate_image(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
     result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
     return result
 
+
 def add_logo(im_origin, alpha_0):
-    rand_mask = np.random.randint(3)
-    
-    logo_s=logo_s_list[rand_mask]
+    rand_mask = np.random.randint(len(logo_s_list))
+
+    logo_s = logo_s_list[rand_mask]
     rot_degree = np.random.randint(-180, 180)
     logo_s = rotate_image(logo_s, rot_degree)
     # RANDOMIZE MASK
@@ -40,14 +44,14 @@ def add_logo(im_origin, alpha_0):
     pixel_shift_y = np.random.randint(-100, 100)
     pixel_zoom_scale = np.random.randint(-3, 4)
     pixel_smooth = np.random.randint(200, 400)
-    flip_up = np.random.randint(9)
+    shifter = np.random.randint(9)
     opacity = np.random.uniform(5, 50) / 100
     # opacity=1.
 
-    if flip_up % 3 != 1:
+    if shifter % 3 != 1:
         logo_s = np.roll(logo_s, pixel_shift_x, axis=1)
 
-    if flip_up % 3 != 2:
+    if shifter % 3 != 2:
         logo_s = np.roll(logo_s, pixel_shift_y, axis=0)
 
     logo_s = smooth_edge(logo_s, pixel_smooth)
@@ -58,20 +62,24 @@ def add_logo(im_origin, alpha_0):
     alpha = logo_s[:, :, 3] / 255.0
 
     alpha *= opacity
-    alpha_0+=alpha
+    blur_kernel = np.random.randint(0, 3)
+    alpha = cv2.blur(alpha, (blur_kernel * 2 + 1, blur_kernel * 2 + 1))
+    alpha_0 += alpha
+
     result[:, :, 0] = (1. - alpha) * im_origin[:, :, 0] + alpha * logo_s[:, :, 0]
     result[:, :, 1] = (1. - alpha) * im_origin[:, :, 1] + alpha * logo_s[:, :, 1]
     result[:, :, 2] = (1. - alpha) * im_origin[:, :, 2] + alpha * logo_s[:, :, 2]
-    
+
     return result, alpha_0
+
 
 def gen_mask(im_origin: np.ndarray):
     # This assume generated text is within Center 256x256 crop
     no_logo = np.random.randint(1, 6)
-    alpha_0=np.zeros((512,512))
+    alpha_0 = np.zeros((512, 512))
     for _ in range(no_logo):
-      im_origin,alpha_0=add_logo(im_origin, alpha_0)
-    
+        im_origin, alpha_0 = add_logo(im_origin, alpha_0)
+
     num_effective_pixel = len(np.where(alpha_0 > 0)[0])
     return im_origin, num_effective_pixel
 
